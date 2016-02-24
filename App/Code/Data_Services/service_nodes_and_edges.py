9ba10061b714f2df67d5358b3e7f7f7c9b463cd8
John Stinson <johnstinson99@gmail.com>
2016-02-24 07:09:35 +0000

    Added png file save and png path into rest service

--- App/Code/Data_Services/service_nodes_and_edges.py
+++ App/Code/Data_Services/service_nodes_and_edges.py
@@ -7,24 +7,30 @@ from flask import Flask, jsonify  # , abort, make_response
 
 from App.Code.Data_NodesAndEdgesFromCSV.create_nodes_and_edges_from_csv import JiraReader
 from App.Code.Data_Services.service_crossdomain_http_utils import crossdomain
+from App.Code.Views_Seaborn.seaborn_analysis import SeabornGenerator
 
-my_dir = 'C:\\Users\\John\\Documents\\2016\\Python\\JiraStates'
-f = join(my_dir, 'jira_states.csv')
-
-my_jira_reader = JiraReader(f)
+my_path = 'C:\\Users\\John\\Documents\\2016\\Python\\JiraStates'
+my_file_and_path = join(my_path, 'jira_states.csv')
+my_jira_reader = JiraReader(my_file_and_path)
 print(my_jira_reader)
+jira_df = my_jira_reader.get_jira_df()
 
 node_list = my_jira_reader.get_unique_node_dict_list()
 print('nodes_list = ' + str(node_list))
 edge_dict_list = my_jira_reader.get_unique_combined_edge_dict_list()
 print('edges_list = ' + str(edge_dict_list))
 
+my_seaborn_gen = SeabornGenerator(my_path, jira_df)
+my_png_dict = my_seaborn_gen.get_filename_dict()
+# my_seaborn_gen.open_files_in_chrome(my_dict)
+print("my_dict = " + str(my_png_dict))
+
 app = Flask(__name__)
 @app.route('/nodes_and_edges')
 # This is the critical line. It calls crossdomain function which allows the calling app to call it on localhost.
 @crossdomain(origin='*')
 def node_service():
-    return jsonify({'nodes': node_list, 'edges': edge_dict_list})
+    return jsonify({'nodes': node_list, 'edges': edge_dict_list, 'pngs': my_png_dict})
 
 
 # @app.route('/todo/api/v1.0/tasks', methods=['GET'])  # all tasks
--- App/Code/Views_Seaborn/seaborn_analysis.py
+++ App/Code/Views_Seaborn/seaborn_analysis.py
@@ -1,10 +1,92 @@
 
-# %matplotlib inline
-import numpy as np
-import pandas as pd
-from scipy import stats, integrate
 import matplotlib.pyplot as plt
+from os.path import join
+from os import system
 import seaborn as sns
-sns.set(color_codes=True)
-np.random.seed(sum(map(ord, "distributions")))
 
+
+class SeabornGenerator:
+    def __init__(self, my_path, my_jira_df):
+        self.filename_dict = {}
+        self.path = my_path
+        self.jira_df = my_jira_df
+        self.set_style()
+        # print("jira_df = ")
+        # print(str(self.jira_df))
+        print("my_path = ")
+        print(self.path)
+
+        # my_dir = 'C:\\Users\\John\\Documents\\2016\\Python\\JiraStates'
+        self.plot_graph1(self.jira_df, "output1", "title output1", self.path, self.filename_dict)
+        self.plot_facet_grid(self.jira_df, "facet_grid", "state transition time analysis facet grid from seaborne", self.path, self.filename_dict)
+        self.plot_graph3(self.jira_df, "output3", "title output3", self.path, self.filename_dict)
+        # self.open_files_in_chrome(self.filename_dict)
+
+    def get_filename_dict(self):
+        return self.filename_dict
+
+    # Load JIRA dataframe
+    # my_csv_file_and_path = join(my_dir, 'jira_states.csv')
+    # jira_df = pd.read_csv(my_csv_file_and_path)
+    # print(jira_df)
+    def set_style(self):
+        sns.set(style="whitegrid", color_codes=True)
+
+    def save_file(self, my_filename_without_path_no_extension, my_chart_title, my_path, my_filename_dict):
+        my_file_and_path = join(my_path, my_filename_without_path_no_extension + ".png")
+        plt.savefig(my_file_and_path)
+        my_dict = {}
+        my_dict["file_and_path"] = my_file_and_path
+        my_dict["title"] = my_chart_title
+        # my_dict["x_pixels"] = 1000
+        # my_dict["y_pixels"] = 1000
+        my_filename_dict[my_filename_without_path_no_extension] = my_dict
+
+    def plot_graph1(self, my_jira_df, my_filename_without_path, my_chart_title, my_path, my_filename_dict):
+        g = sns.PairGrid(my_jira_df, vars=["DayDiff", "DateNum"])  # , hue="IssueType"
+        g.map(plt.scatter)
+        # print("my_filename_without_path = " + my_filename_without_path)
+        # print("my_path = " + my_path)
+        # print("my_filename_dict = " + str(my_filename_dict))
+        self.save_file(my_filename_without_path, my_chart_title, my_path, my_filename_dict)
+
+    def plot_facet_grid(self, my_jira_df, my_filename_without_path, my_chart_title, my_path, my_filename_dict):
+        with sns.axes_style("white"):
+            '''g = sns.FacetGrid(
+                    my_jira_df,
+                    row="IssueType",
+                    col="From",
+                    margin_titles=True,
+                    size=2.5,
+                    hue = 'To')'''
+            g = sns.FacetGrid(
+                    my_jira_df,
+                    row="To",
+                    col="From",
+                    margin_titles=True,
+                    size=2.5,
+                    hue='IssueType')
+            g.map(plt.scatter,
+                  "WeekNum",
+                  "DayDiff",
+                  color="#334488",
+                  edgecolor="white",
+                  lw=.5)
+            g.set_axis_labels("Week Number", "Days in State")
+            g.set(xticks=list(range(0, 101, 20)), yticks=list(range(0, 101, 20)))
+            g.fig.subplots_adjust(wspace=.02, hspace=.02)
+
+        self.save_file(my_filename_without_path, my_chart_title, my_path, my_filename_dict)
+
+    def plot_graph3(self, my_jira_df, my_filename_without_path, my_chart_title, my_path, my_filename_dict):
+        sns.swarmplot(x="From", y="DayDiff", hue="To", data=my_jira_df)
+        self.save_file(my_filename_without_path, my_chart_title, my_path, my_filename_dict)
+
+    # print("chrome_file = " + chrome_file)
+    # print("png_file = " + my_png_file_and_path)
+    # call(["dir"])
+
+    def open_files_in_chrome(self, filename_dict):
+        chrome_file = '"c:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"'
+        for my_file in filename_dict.values():
+            system(chrome_file + " " + my_file)
